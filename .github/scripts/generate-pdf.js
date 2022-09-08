b742fa794b7780809999430b6cd7d89fbedf67eb
root <remy.vanherweghem@parl.gc.ca>
2022-09-08 17:12:24 +0000

    Updated preview index page margins.

--- .github/scripts/generate-pdf.js
+++ .github/scripts/generate-pdf.js
@@ -1,23 +1,29 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+const makePdf = async function (language) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    let rootFile = language === 'en' ? "index.html" : 'index.fr.html';
     await page.goto('https://pboml-parser--parseur-pboml.s3.ca-central-1.amazonaws.com/index.html', {
         waitUntil: 'networkidle2',
     });
     await page.pdf({
-        path: 'pbomlexample.pdf',
+        path: `pboml-gen-${language}.pdf`,
         format: 'Letter',
         margin: {
-            top: "2cm",
-            left: "2cm",
-            bottom: "2cm",
-            right: "2cm",
+            top: "1.2cm",
+            left: "1.2cm",
+            bottom: "1.2cm",
+            right: "1.2cm",
         },
         displayHeaderFooter: true,
-        footerTemplate: `<div class='text-xs'><span class='pageNumber'></span> / <span class='totalPages'></span></div>`
+        footerTemplate: `<div class='text-xs'><span class='pageNumber'></span> / <span class='totalPages'></span></div>`,
     });
 
     await browser.close();
+};
+
+(async () => {
+    await makePdf("en");
+    await makePdf("fr");
 })();
