971b9089d355881893b3b016a2891cf6d2b08574
root <remy.vanherweghem@parl.gc.ca>
2023-01-31 19:25:18 +0000

    chore: added missing file.

---
+++ src/components/Editor/TinyButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="rounded  text-sm  w-fit p-1" :class="{
+        'bg-red-100 hover:bg-red-300 text-red-800': danger,
+        'bg-blue-100 hover:bg-blue-300 text-blue-800': !danger,
+    }">
+
+        <slot></slot>
+
+
+    </button>
+</template>
+<script>
+export default {
+    props: ['danger']
+}
+</script>
