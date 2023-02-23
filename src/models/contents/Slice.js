e53b573dfc00655d3b4b5a07387e0fd4185e5df2
root <remy.vanherweghem@parl.gc.ca>
2023-02-23 14:34:26 +0000

    feature: support for presentation types and prepared for internal references..

--- src/models/contents/Slice.js
+++ src/models/contents/Slice.js
@@ -4,14 +4,18 @@ import SliceLabelEditor from '../../components/Editor/SliceLabelEditor.js';
 import ChoiceRenderer from '../../components/Editor/Inputs/ChoiceRenderer.js';
 
 export default class Slice {
-    constructor(payload) {
+    constructor(payload, sequence = 0, depth = 0) {
         this.id = payload.id;
         this.type = payload.type;
         this.readonly = payload.readonly;
         this.display_label = payload.display_label;
-        this.is_figure = payload.is_figure ? payload.is_figure : false;
+        this.presentation = payload.presentation ? payload.presentation : null;
         this.choices = payload.choices;
         this.print_only = payload.print_only;
+        this.referenced_as = {
+            en: payload.referenced_as?.en,
+            fr: payload.referenced_as?.fr
+        };
         this.label = {
             en: payload.label?.en,
             fr: payload.label?.fr
@@ -21,6 +25,9 @@ export default class Slice {
         this.state = {
             isEditingMeta: false
         }
+
+        this.sequence = sequence;
+        this.depth = depth;
     }
 
     _renderLabelTitleVnode(language, force = false) {
@@ -29,12 +36,12 @@ export default class Slice {
         let labelNodeType;
         let labelNodeClasses = ["font-thin break-after-avoid"]
         let labelNodeContent = this.label[language];
-        if (this.is_figure) {
+        if (this.presentation === 'figure') {
             labelNodeType = "figcaption";
             labelNodeClasses.push("text-center text-xl")
 
-            if (typeof this.is_figure === "number") {
-                labelNodeContent = `Figure ${this.is_figure} &ndash; ${labelNodeContent}`;
+            if (this.presentation === "figure" && this.referenced_as) {
+                labelNodeContent = `<b>${this.referenced_as[language]}</b> &ndash; ${labelNodeContent}`;
             }
 
         } else {
@@ -88,8 +95,8 @@ export default class Slice {
     renderAsVnode(language = document.documentElement.lang) {
         let classes = ["flex flex-col gap-4 print:mt-4"];
         classes.push(this.print_only ? 'hidden print:flex' : 'flex')
-        classes.push(this.is_figure ? "bg-gradient-to-tr  from-white to-gray-50 rounded-tr-3xl pt-4 py-4 break-inside-avoid-page" : "");
-        return h(this.is_figure ? 'figure' : 'section', { class: classes.join(" ") }, this._buildVnodes(language));
+        classes.push(this.presentation === "figure" ? "bg-gradient-to-tr  from-white to-gray-50 rounded-tr-3xl pt-4 py-4 break-inside-avoid-page" : "");
+        return h(this.presentation === 'figure' ? 'figure' : 'section', { class: classes.join(" ") }, this._buildVnodes(language));
     }
 
     renderEditingVnode() {
@@ -105,8 +112,9 @@ export default class Slice {
             id: this.id,
             readonly: this.readonly,
             display_label: this.display_label,
-            is_figure: this.is_figure,
+            presentation: this.presentation,
             choices: this.choices,
+            referenced_as: this.referenced_as,
             label: {
                 en: this.label?.en,
                 fr: this.label?.fr
