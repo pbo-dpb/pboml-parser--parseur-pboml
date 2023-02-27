7a0dc529f5d2643caa308a7d3099e9753fda435e
root <remy.vanherweghem@parl.gc.ca>
2023-02-27 21:15:00 +0000

    Added readonly spport for notes and sources.

--- src/models/contents/Slice.js
+++ src/models/contents/Slice.js
@@ -1,4 +1,4 @@
-import { h, Suspense } from 'vue'
+import { h, setTransitionHooks, Suspense } from 'vue'
 import LoadingIndicator from "../../components/LoadingIndicator.vue"
 import SliceLabelEditor from '../../components/Editor/SliceLabelEditor.js';
 import ChoiceRenderer from '../../components/Editor/Inputs/ChoiceRenderer.js';
@@ -12,15 +12,33 @@ export default class Slice {
         this.presentation = payload.presentation ? payload.presentation : null;
         this.choices = payload.choices;
         this.print_only = payload.print_only;
+
         this.referenced_as = {
             en: payload.referenced_as?.en,
             fr: payload.referenced_as?.fr
         };
+
         this.label = {
             en: payload.label?.en,
             fr: payload.label?.fr
         }
+
+        this.sources = payload.sources ? payload.sources.map(src => {
+            return {
+                en: src?.en,
+                fr: src?.fr
+            }
+        }) : [];
+
+        this.notes = payload.notes ? payload.notes.map(src => {
+            return {
+                en: src?.en,
+                fr: src?.fr
+            }
+        }) : [];
+
         this.content = payload.content;
+
 
         this.state = {
             isEditingMeta: false,
@@ -53,11 +71,41 @@ export default class Slice {
         return h(labelNodeType, { innerHTML: labelNodeContent, class: labelNodeClasses.join(" ") });
     }
 
+    __renderMetaVnodes(label, content, language) {
+        return [
+            h('dl', { class: 'flex flex-col grid-cols-3 gap-.5 border-l-2 border-gray-200 dark:border-gray-800 pl-2 ' }, [
+                h('dt', { class: "text-xs font-semibold", innerHTML: label }),
+                h('dd', { class: "prose-sm ", innerHTML: content.map(src => src[language]).join("; ") }),
+            ]),
+
+        ]
+    }
+
+    _renderSourcesVnodes(language) {
+        if (!this.sources.length) return [];
+        return this.__renderMetaVnodes((this.sources.length > 1 ? 'Sources' : 'Source'), this.sources, language);
+    }
+
+    _renderNotesVnodes(language) {
+        if (!this.notes.length) return [];
+        return this.__renderMetaVnodes(this.notes.length > 1 ? 'Notes' : 'Note', this.notes, language);
+    }
+
+    _renderMetaVnodes(language) {
+        if (!this.sources.length && !this.notes.length) return [];
+
+        return h('div', { 'class': 'flex flex-col gap-2' }, [
+            ...this._renderSourcesVnodes(language),
+            ...this._renderNotesVnodes(language)
+        ])
+    }
+
 
     _buildVnodes(language) {
         return [
             this._renderLabelTitleVnode(language),
-            this.renderReadonlyVnode(language)
+            this.renderReadonlyVnode(language),
+            this._renderMetaVnodes(language)
         ];
     }
 
@@ -96,7 +144,7 @@ export default class Slice {
     renderAsVnode(language = document.documentElement.lang) {
         let classes = ["flex flex-col gap-4 print:mt-4"];
         classes.push(this.print_only ? 'hidden print:flex' : 'flex')
-        classes.push(this.presentation === "figure" ? "mx-auto w-full xl:w-2/3 bg-gradient-to-tr from-white to-gray-50 rounded-tr-3xl py-4 break-inside-avoid-page" : "");
+        classes.push(this.presentation === "figure" ? "mx-auto w-full xl:w-2/3 bg-gradient-to-tr from-white to-gray-100 rounded-tr-3xl py-4 break-inside-avoid-page" : "");
         classes.push(this.presentation === "aside" ? "bg-gradient-to-tr from-sky-100 to-sky-50  rounded-tr-3xl p-4 break-inside-avoid-page" : "");
 
         let elType = 'section';
@@ -130,6 +178,8 @@ export default class Slice {
                 en: this.label?.en,
                 fr: this.label?.fr
             },
+            sources: this.sources,
+            notes: this.notes,
             content: this.content
         }
     }
