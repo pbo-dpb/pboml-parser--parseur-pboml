4c1d44113160e9690feb678d4f5c46bee5668b52
root <remy.vanherweghem@parl.gc.ca>
2023-03-29 14:14:42 +0000

    feature: pboml figure remove maximum width

--- src/models/contents/GraphSlice.js
+++ src/models/contents/GraphSlice.js
@@ -30,7 +30,7 @@ export default class GraphSlice extends Slice {
     renderReadonlyVnode(language) {
         return h(Suspense, null, {
             default: () => h('div', { class: 'flex print:block flex-col items-center justify-center' }, [
-                h('div', { class: `w-full ${this.presentation === 'figure' ? '' : 'xl:w-2/3'}` }, [
+                h('div', { class: `w-full'}` }, [
                     h(Grapher, {
                         language: language,
                         strings: this.strings,
