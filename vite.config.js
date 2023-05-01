4c459959a11762061fbe049c47e790812ee154f7
root <remy.vanherweghem@parl.gc.ca>
2023-05-01 16:51:16 +0000

    refactor: removed temporary self signing plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const path = require('path')
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
   base: "./",
-  plugins: [mkcert(), vue({
+  plugins: [vue({
     compilerOptions: {
       customElement: true,
     },
