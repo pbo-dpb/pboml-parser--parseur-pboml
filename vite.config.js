709f65650d15a2fda925c9751acbc3d41f49d0dc
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-06-08 18:42:20 +0000

    reverted back to non-module.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
