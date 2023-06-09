975785356790f6c4bd97e4bf61b5d592db8bf678
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-06-09 12:26:38 +0000

    feature: continued customizing print css.

--- src/Renderers/Html/SliceHtmlRenderer.js
+++ src/Renderers/Html/SliceHtmlRenderer.js
@@ -163,8 +163,8 @@ export default class SliceHtmlRenderer {
     renderAsVnode(language = document.documentElement.lang) {
         let classes = ["flex flex-col gap-4 print:mt-4 @container/slice"];
         classes.push(this.print_only ? 'hidden print:flex' : 'flex')
-        classes.push(this.presentation === "figure" ? "bg-gradient-to-tr from-transparent to-zinc-100 dark:to-zinc-800 rounded-tr-3xl p-4 break-inside-avoid-page" : "");
-        classes.push(this.presentation === "aside" ? "bg-gradient-to-tr from-sky-50 to-sky-100 dark:from-transparent dark:to-sky-900 rounded-tr-3xl p-4 break-inside-avoid-page" : "");
+        classes.push(this.presentation === "figure" ? "bg-gradient-to-tr from-transparent to-zinc-100 dark:to-zinc-800 rounded-tr-3xl p-4 break-inside-avoid-page pb__figure" : "");
+        classes.push(this.presentation === "aside" ? "bg-gradient-to-tr from-sky-50 to-sky-100 dark:from-transparent dark:to-sky-900 rounded-tr-3xl p-4 break-inside-avoid-page  pb__aside" : "");
 
         let elType = 'section';
         if (this.presentation === 'figure') elType = 'figure';
