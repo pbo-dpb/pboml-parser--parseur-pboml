4cad3c029059f95eb1ea08388d0c9d5936df6ca2
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-08-28 19:15:13 +0000

    Attempted to fix issue with negative number signs.

--- src/components/Editor/Inputs/NumberInput.vue
+++ src/components/Editor/Inputs/NumberInput.vue
@@ -2,13 +2,29 @@
     <fieldset :class="label ? `flex flex-col p-1 gap-0.5 border border-gray-300 rounded` : ''">
         <legend v-if="label" class="font-semibold px-1">{{ label }}</legend>
 
-        <input class="border border-gray-300 p-1 w-full rounded" :value="modelValue" type="number"
-            @input="$emit('update:modelValue', parseFloat($event.target.value))" />
+        <input ref="input" class="border border-gray-300 p-1 w-full rounded" :value="modelValue" type="number"
+            @input="handleInput" @blur="handleBlur" />
 
         <span v-if="hint" class="text-sm italic text-gray-700 dark:text-gray-300">{{ hint }}</span>
     </fieldset>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue'
+
 defineProps(['modelValue', 'label', 'hint'])
-defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const input = ref(null)
+
+function handleInput(e) {
+    let value = e.target.value;
+    if (value !== '' && value != '-') {
+        emit('update:modelValue', parseFloat(value))
+    }
+}
+
+function handleBlur(e) {
+    let value = e.target.value;
+    emit('update:modelValue', value ? parseFloat(value) : null)
+}
 </script>
