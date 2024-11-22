de9ae7d32974b3721b9f23ad93eff8b23f42caaa
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2024-11-22 00:49:41 +0000

    Fixed vite warning.

--- src/components/Editor/DataTableEditor/DataTableDisplayEditor/DataTableDisplayEditorDataEntryGroups/DataTableDisplayEditorDataEntryGroupsGroup.vue
+++ src/components/Editor/DataTableEditor/DataTableDisplayEditor/DataTableDisplayEditorDataEntryGroups/DataTableDisplayEditorDataEntryGroupsGroup.vue
@@ -15,15 +15,17 @@
 
         <div class="bg-cyan-50 p-4 w-full" v-if="encompasses">
             <table class="w-full">
-                <tr>
-                    <td v-for="entry in encompasses" :width="`${100 / totalNumberOfEntries}%`"
-                        class=" text-xs text-center p-2" :class="{
-                            'font-semibold border-t-4 border-t-cyan-800': entry.isInRange,
-                            'border-t-4 border-t-gray-300': !entry.isInRange
-                        }">
-                        {{ entry.label }}
-                    </td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td v-for="entry in encompasses" :width="`${100 / totalNumberOfEntries}%`"
+                            class=" text-xs text-center p-2" :class="{
+                                'font-semibold border-t-4 border-t-cyan-800': entry.isInRange,
+                                'border-t-4 border-t-gray-300': !entry.isInRange
+                            }">
+                            {{ entry.label }}
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
 
@@ -34,7 +36,7 @@
     </div>
 </template>
 <script setup>
-import { ref, defineEmits, computed } from 'vue';
+import { ref, computed } from 'vue';
 import { TrashIcon } from '@heroicons/vue/16/solid';
 import editorStrings from '../../../../../editor-strings.js';
 import DataTableEntryGroup from '../../../../../models/contents/DataTable/DataTableEntryGroup.js';
