2c6af4afb01073e5c24e25ee60a1089b65ce5cc7
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2025-07-08 16:13:21 +0000

    chore: new citation-js.

--- src/components/Renderer/AnnotationBibtex.js
+++ src/components/Renderer/AnnotationBibtex.js
@@ -1,26 +1,40 @@
-import Cite from "citation-js";
+
+import { Cite } from '@citation-js/core'
+import "@citation-js/plugin-bibtex";
+import "@citation-js/plugin-csl";
+import { data } from 'happy-dom/lib/PropertySymbol.js';
 import { h } from "vue"
 
 
 export default {
     props: ['annotation', 'language'],
+    data() {
+        return {
+            _citation: null,
+        }
+    },
     render() {
 
-        let output;
-        try {
-            const reference = new Cite(this.annotation.content[this.language]);
-            output = reference.format('bibliography', {
-                format: 'html',
-                template: 'apa',
-                lang: `${this.language}-CA`
-            })
-            return h('div', { innerHTML: output });
-        } catch (error) {
+        if (this._citation === null) {
+            Cite.async(this.annotation.content[this.language]).then((reference) => {
 
+                this._citation = reference.format('bibliography', {
+                    format: 'html',
+                    template: 'apa',
+                    lang: `${this.language}-CA`
+                })
+
+            }, (error) => {
+                console.error("Error parsing citation:", error);
+                this._citation = false;
+            });
+
+            return h('div', {}, '');
         }
 
 
-        output = h('div', { class: "font-mono text-red-500" }, this.annotation.content[this.language]);
+        return this._citation ? h('div', { innerHTML: this._citation }) : h('div', { class: "font-mono text-red-500", innerHTML: this.annotation.content[this.language] ? this.annotation.content[this.language] : this.annotation.content });
 
-    }
+    },
+
 }
